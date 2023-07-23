14f3307b03c43b024c2c0e6e58b4fb3e04ee7f51
k3ru-sailco <65882535+k3ru-sailco@users.noreply.github.com>
2023-07-22 20:47:01 -0700

    better example

--- example.go
+++ example.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	tcp_data "github.com/k3ru-sailco/request-fp/lib"
+	request_fp "github.com/k3ru-sailco/request-fp/lib"
 )
 
 func main() {
 	/* start listening on the wifi network interface (i am using a macbook in this example)*/
-	go tcp_data.StartListening("en0")
+	go request_fp.StartListening("en0")
 	router := fiber.New()
-	router.Get("/data", func(c *fiber.Ctx) error {
-		/* returns all the fingerprint data from incoming packets */
-		return c.JSON(tcp_data.FINGERPRINTS)
+	router.Get("/fingerprint-me", func(c *fiber.Ctx) error {
+		/* returns the fingerprint of the incoming request */
+		data, _ := request_fp.RequestFP(c.IP())
+		return c.JSON(data)
 	})
 	router.Listen(":8080")
 }
